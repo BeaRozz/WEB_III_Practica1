9b61044e512592959fd43c392393f1cd8496abb6
BeaRozz <asucamedina@gmail.com>
2025-09-19 18:31:21 -0600

    Clase 19/09/25 �

--- backend/test_main.py
+++ backend/test_main.py
@@ -1,17 +1,16 @@
 import pytest
 import mongomock
 
-from pytest import monkeypatch
 from pymongo import MongoClient
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
 import main
 
-client = TestClient(app)
-mongo_client = mongomock.MongoClient()
-database = mongo_client.practica1
-collection_historial = database.historial
+client = TestClient(main.app)
+fake_mongo_client = mongomock.MongoClient()
+fake_database = fake_mongo_client.practica1
+fake_collection_historial = fake_database.historial
 
 @pytest.mark.parametrize(
         "numeroa, numerob, resultado",
@@ -26,10 +25,34 @@ collection_historial = database.historial
 )
 
 def test_sumar(monkeypatch, numeroa, numerob, resultado):
-    monkeypatch.setattr(main, "collection_historial", collection_historial)
+    monkeypatch.setattr(main, "collection_historial", fake_collection_historial)
     
     response = client.get(f"/calculadora/sum?a={numeroa}&b={numerob}")
     assert response.status_code == 200
     assert response.json() == {"a": numeroa, "b": numerob, "resultado": resultado}
     
-    assert collection_historial.find_one({"resultado": resultado, "a": numeroa, "b": numerob}) is not None
+    assert fake_collection_historial.find_one({"resultado": resultado, "a": numeroa, "b": numerob}) is not None
+
+def test_historial(monkeypatch):
+    monkeypatch.setattr(main, "collection_historial", fake_collection_historial)
+
+    response = client.get("/calculadora/historial")
+    assert response.status_code == 200
+
+    #Obtenemos todos los documentos que ya fueron ingresados
+    expected_data = list(fake_collection_historial.find({}))
+
+    historial = []
+    for document in expected_data:
+        historial.append({
+            "a": document["a"],
+            "b": document["b"],
+            "resultado": document["resultado"],
+            "date": document["date"].isoformat()
+        })
+    
+    print(f"DEBUG: exected_data{historial}")
+    print(f"DEBUG: response.json{response.json()}")
+
+    #Comparamos las respuestas
+    assert response.json() == {"historial": historial}
