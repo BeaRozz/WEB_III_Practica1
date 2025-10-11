4aee59e5b6491f99d0b0a2c79c941ffa93225de3
BeaRozz <asucamedina@gmail.com>
2025-10-10 18:02:37 -0600

    Workflow Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,7 +25,7 @@ function App() {
 
   return (
     <div style={{ padding: 20 }}>
-      <h1>Calculadora Tiene Cambios</h1>
+      <h1>Calculadora Tiene Cambios, Sí</h1>
 
       <input
         type="number"
